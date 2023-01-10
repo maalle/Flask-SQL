884ac9b5e6b10f0270a2f377f1f46e6126a0f46a
maalle <36455418+maalle@users.noreply.github.com>
2023-01-10 12:20:27 +0200

    Add files via upload

---
+++ Ruokalista.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ruokalista(id integer private key, nimi string, hinta
+decimal(5, 2));
+
+INSERT INTO ruokalista(id, nimi, hinta) VALUES(1, 'Kaurapuuro', 2.50);
+INSERT INTO ruokalista(id, nimi, hinta) VALUES(2, 'Pyttipannu', 5.50);
+INSERT INTO ruokalista(id, nimi, hinta) VALUES(3, 'Kaalikääryleet', 10.90);
+INSERT INTO ruokalista(id, nimi, hinta) VALUES(4, 'Lohikeitto', 8.90);
